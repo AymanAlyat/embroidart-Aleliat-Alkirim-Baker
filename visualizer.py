9ec3f1c479efee7c2ca1f1c7606afca00464b9d3
Ayman Alyat <aymanaleliat@gmail.com>
2026-06-01 23:07:49 +0300

    add visualizer class

---
+++ visualizer.py
@@ -0,0 +1,13 @@
+import matplotlib.pyplot as plt
+
+
+class Visualizer:
+
+    def preview_pattern(self, stitches, title="Embroidery Pattern"):
+        pass
+
+    def compare_original_and_pattern(self, original_image, stitches):
+        pass
+
+    def show_thread_colors(self, colors):
+        pass
