6764a2a92af37c35677937947d4992b2d5985cc8
Ahmed Alkirim <ahmadalqerem96@gmail.com>
2026-06-04 18:16:48 +0300

    Initialize blank image canvas using PIL

--- test_image.py
+++ test_image.py
@@ -3,3 +3,7 @@ from PIL import Image, ImageDraw
 
 def create_test_image(output_path="test_flower.png"):
     pass
+
+
+img = Image.new("RGB", (200, 200), (255, 255, 255))
+draw = ImageDraw.Draw(img)
